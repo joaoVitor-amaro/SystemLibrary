f044cf7ead1dc921d29721e6cab2b14690b7a793
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-11 13:38:11 -0300

    feat: Adicionado a rota do login e da página home do bibliotecario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { HomeBibliotecario } from './pages/bibliotecario/home-bibliotecario/home-bibliotecario';
+import { AuthGuard } from './service/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Login},
+    {path: 'home-bibliotecario', component: HomeBibliotecario, canActivate: [AuthGuard]}
+];

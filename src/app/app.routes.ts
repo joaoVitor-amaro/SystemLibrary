1f242a79d4cf5d69812079ca07c760b6b564071f
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-13 22:32:25 -0300

    feat: adicionar a rota de cadastrar o aluno e a proteção nela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { HomeBibliotecario } from './pages/bibliotecario/home-bibliotecario/home-bibliotecario';
 import { AuthGuard } from './service/auth.guard';
+import { CadasterAluno } from './pages/bibliotecario/cadaster-aluno/cadaster-aluno';
 
 export const routes: Routes = [
     {path: '', component: Login},
-    {path: 'home-bibliotecario', component: HomeBibliotecario, canActivate: [AuthGuard]}
+    {path: 'home-bibliotecario', component: HomeBibliotecario, canActivate: [AuthGuard]},
+    {path: 'cadaster-aluno', component: CadasterAluno, canActivate: [AuthGuard]}
 ];

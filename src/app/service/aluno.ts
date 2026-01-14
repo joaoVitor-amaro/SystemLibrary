a4d250cd283bcb297e2d311712c9c7f73066318e
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-13 22:31:33 -0300

    feat: Criar a estrutura do Aluno na request da Api

---
+++ src/app/service/aluno.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Aluno } from '../models/aluno';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AlunoCadastroResponse } from '../models/aluno-cadastro-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlunoService {
+  private urlApi: string = 'http://localhost:8080/bibliotecario/cadastrarAluno';
+
+  constructor(private http: HttpClient) {}
+  
+  cadastrar(aluno: Aluno): Observable<AlunoCadastroResponse> {
+    return this.http.post<AlunoCadastroResponse>(this.urlApi, aluno);
+  }
+}

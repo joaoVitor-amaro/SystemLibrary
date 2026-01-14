690da7d931f339d96e34b35abce23b80dd5535c0
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-13 23:36:13 -0300

    fix: remove lógica de contagem no sidebar

--- src/app/component/sidebar/sidebar.ts
+++ src/app/component/sidebar/sidebar.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../service/auth';
 import { Usuario } from '../../models/usuario';
 
@@ -7,17 +7,14 @@ import { Usuario } from '../../models/usuario';
   selector: 'app-sidebar',
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css'],
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class Sidebar {
   bibliotecario: Usuario | null = null
-  activeIndex = 0;
 
   constructor(private router: Router, private auth: AuthService){
       this.bibliotecario = this.auth.getUser();
   }
-
-  setActive(i: number) { this.activeIndex = i; }
 
   logout(): void {
       this.auth.logout();

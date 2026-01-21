3e36d382fe87caa1ef835191f075056ee3167cc4
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-21 13:14:48 -0300

    feat: Adicionar a rota dos dados do bibliotecario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Login } from './pages/login/login';
 import { HomeBibliotecario } from './pages/bibliotecario/home-bibliotecario/home-bibliotecario';
 import { AuthGuard } from './service/auth.guard';
 import { CadasterAluno } from './pages/bibliotecario/cadaster-aluno/cadaster-aluno';
+import { DadosBibliotecario } from './pages/bibliotecario/dados-bibliotecario/dados-bibliotecario';
 
 export const routes: Routes = [
     {path: '', component: Login},
     {path: 'home-bibliotecario', component: HomeBibliotecario, canActivate: [AuthGuard]},
-    {path: 'cadaster-aluno', component: CadasterAluno, canActivate: [AuthGuard]}
+    {path: 'cadaster-aluno', component: CadasterAluno, canActivate: [AuthGuard]},
+    {path: 'dados-bibliotecario', component: DadosBibliotecario, canActivate: [AuthGuard]}
 ];

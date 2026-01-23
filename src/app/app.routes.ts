b4df1aef577718adb9ae144bb4882e0c0a2d5b20
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:47:49 -0300

    feat: adicionar a rota cadastro do bibliotecario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeBibliotecario } from './pages/bibliotecario/home-bibliotecario/home
 import { AuthGuard } from './service/auth.guard';
 import { CadasterAluno } from './pages/bibliotecario/cadaster-aluno/cadaster-aluno';
 import { DadosBibliotecario } from './pages/bibliotecario/dados-bibliotecario/dados-bibliotecario';
+import { CadastroBibliotecario } from './pages/admin/cadastro-bibliotecario/cadastro-bibliotecario';
 
 export const routes: Routes = [
     {path: '', component: Login},
     {path: 'home-bibliotecario', component: HomeBibliotecario, canActivate: [AuthGuard]},
     {path: 'cadaster-aluno', component: CadasterAluno, canActivate: [AuthGuard]},
-    {path: 'dados-bibliotecario', component: DadosBibliotecario, canActivate: [AuthGuard]}
+    {path: 'dados-bibliotecario', component: DadosBibliotecario, canActivate: [AuthGuard]},
+    {path: 'cadastro-bibliotecario', component: CadastroBibliotecario}
 ];

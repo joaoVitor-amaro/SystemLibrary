e4ea7659484c9b7ea807a5c8e3c1dee905e5f564
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-01-22 23:46:35 -0300

    feat: Criado o service para fazer a requisição da api de cadastro do bibliotecario

---
+++ src/app/service/admin.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BibliotecarioRequest } from '../models/bibliotecario-request';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../models/api-response';
+import { BibliotecarioDTO } from '../models/bibliotecario-dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  private api: string = 'http://localhost:8080/admin/cadastrarBibliotecario';
+
+  constructor(private http: HttpClient) {}
+
+  cadastrarBibliotecario(request: BibliotecarioRequest): Observable<ApiResponse<BibliotecarioDTO>>{ {
+    return this.http.post<ApiResponse<BibliotecarioDTO>>(this.api, request);
+  }
+
+  }
+  
+}
